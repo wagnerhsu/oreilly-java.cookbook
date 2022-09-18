2c115534b8a05545b5ac749dcc74bd79c45a8ff0
Wagner Xu <wagnerhsu@hotmail.com>
2022-09-18 21:26:10 +0800

    add file

---
+++ MyCode/17-Reflection/ReflectionDemo/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.logging.Logger;
+
+public class Main {
+    static Logger _logger = Logger.getLogger(Main.class.getName());
+    public static void main(String[] args) {
+        _logger.info("Hello");
+    }
+}
