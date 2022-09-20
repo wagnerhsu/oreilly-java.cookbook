4a15229d81c5309a82e4da952b3aed56c7c8e321
Wagner Xu <wagnerhsu@hotmail.com>
2022-09-20 09:22:22 +0800

    update pom.xml to use java 1.11

--- MyCode/17-Reflection/ReflectionDemo/reflection/src/main/java/com/wagner/App.java
+++ MyCode/17-Reflection/ReflectionDemo/reflection/src/main/java/com/wagner/App.java
@@ -1,5 +1,7 @@
 package com.wagner;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,5 +16,37 @@ public class App {
         logger.info(App.class.getName());
         logger.info(App.class.getSimpleName());
         logger.info(App.class.getCanonicalName());
+
+        try {
+            Class<?> c1 = Class.forName("com.wagner.App");
+            App c2 = (App) c1.getDeclaredConstructor().newInstance();
+            c2.display();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public void display() {
+        logger.info("App.display");
     }
 }
