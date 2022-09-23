b362c418c9223f2bdf28c5fa1bf94d5791e8fdd7
Wagner Xu <wagnerhsu@hotmail.com>
2022-09-23 09:36:34 +0800

    update files

---
+++ MyCode/14-JsonData/JacksonDemo/README.md
@@ -0,0 +1,5 @@
+# README
+
+## 2022-09-23
+
+* 默认的ObjectMapper必须是大小写匹配，如果想进行更多的定制，可以使用`JsonMapper.builder()`
--- MyCode/14-JsonData/JacksonDemo/src/main/java/com/wagner/App.java
+++ MyCode/14-JsonData/JacksonDemo/src/main/java/com/wagner/App.java
@@ -5,7 +5,10 @@ import org.apache.logging.log4j.Logger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 /**
  * Hello world!
@@ -17,11 +20,36 @@ public class App {
     public static void main(String[] args) throws JsonProcessingException {
         ObjectToJsonString();
         JsonStringToObject();
+        JsonStringToObjectUsingJsonMapper();
+        ObjectToJsonStringUsingJsonMapper();
+    }
+
+    private static void ObjectToJsonStringUsingJsonMapper() throws JsonProcessingException {
+        logger.debug(">>ObjectToJsonStringUsingJsonMapper");
+        User user = new User();
+        user.name = "Wagner";
+        user.car = new Car();
+        user.car.model = "BMW";
+        ObjectMapper mapper = JsonMapper.builder().enable(SerializationFeature.INDENT_OUTPUT).build();
+        String jsonString = mapper.writeValueAsString(user);
+        logger.info(jsonString);
+        logger.debug("<<ObjectToJsonStringUsingJsonMapper");
+    }
+
+    private static void JsonStringToObjectUsingJsonMapper() throws JsonMappingException, JsonProcessingException {
+        logger.debug(">>JsonStringToObjectUsingJsonMapper");
+        String jsonString = "{\"Name\":\"Wagner\",\"car\":{\"model\":\"BMW\"}}";
+        ObjectMapper mapper = JsonMapper.builder().enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES).build();
+
+        User user = mapper.readValue(jsonString, User.class);
+        logger.info(String.format("%s %s", user.name, user.car.model));
+        logger.debug("<<JsonStringToObjectUsingJsonMapper");
     }
 
     private static void JsonStringToObject() throws JsonMappingException, JsonProcessingException {
         String jsonString = "{\"name\":\"Wagner\",\"car\":{\"model\":\"BMW\"}}";
         ObjectMapper mapper = new ObjectMapper();
+
         User user = mapper.readValue(jsonString, User.class);
         logger.info(String.format("%s %s", user.name, user.car.model));
     }
